20ee6c4b47487f2f831c5d98519b7b0aa3059241
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2024-02-16 15:28:19 -0500

    added all the PWA files

--- sw.js
+++ sw.js
@@ -3,12 +3,12 @@ const cacheName = `YourNameHere-${version}`;
 const staticFiles = [];
 
 self.addEventListener('install', (ev) => {
-    //if you have an array of files then addAll() here
-    ev.waitUntil(
-      caches.open(cacheName).then((cache) => {
-        cache.addAll(staticFiles);
-      })
-    );
+  //if you have an array of files then addAll() here
+  ev.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      cache.addAll(staticFiles);
+    })
+  );
 });
 self.addEventListener('activate', (ev) => {
   //delete old version
@@ -18,8 +18,10 @@ self.addEventListener('activate', (ev) => {
     })
   );
 });
+
 self.addEventListener('fetch', (ev) => {
   //try the cache first, then fetch and save copy in cache
+  ev.respondWith(cacheFirstAndSave(ev));
 });
 
 function cacheFirst(ev) {
@@ -30,6 +32,29 @@ function cacheFirst(ev) {
 }
 function cacheFirstAndSave(ev) {
   //try cache then fetch
+  return caches.match(ev.request).then((cacheResponse) => {
+    return (
+      cacheResponse ||
+      fetch(ev.request)
+        .then((fetchResp) => {
+          if (fetchResp.status > 0 && !fetchResp.ok) throw new Error('bad response');
+          //throw the error IF NOT opaque AND NOT in the 200-299 range
+          //we will just return a 404 error... to keep it simple
+          return caches
+            .open(cacheName)
+            .then((cache) => {
+              return cache.put(ev.request, fetchResp.clone());
+            })
+            .then(() => {
+              //finished putting the copy in the cache
+              return fetchResp; //send the resp to the browser
+            });
+        })
+        .catch((err) => {
+          return response404();
+        })
+    );
+  });
 }
 function response404() {
   //any generic 404 error that we want to generate
